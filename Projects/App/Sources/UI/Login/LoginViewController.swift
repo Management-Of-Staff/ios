e976315178994c1e147ae17c2ab431ea61c21cb9
Taehwan Kim <obtusa07@gmail.com>
2023-04-07 19:34:16 +0900

    Fix: Modify to build on device

--- Projects/App/Sources/UI/Login/LoginViewController.swift
+++ Projects/App/Sources/UI/Login/LoginViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Combine
 import DesignSystem
+import SwiftUI
 
 final class LoginViewController: UIViewController {
     
@@ -289,3 +290,59 @@ extension LoginViewController {
         case hide
     }
 }
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        LoginViewController()
+            .showPreview(.iPhone12Pro)
+            
+    }
+}
+#endif
+
+
+enum DeviceType {
+    case iPhoneSE2
+    case iPhone8
+    case iPhone12Pro
+    case iPhone12ProMax
+
+    func name() -> String {
+        switch self {
+        case .iPhoneSE2:
+            return "iPhone SE"
+        case .iPhone8:
+            return "iPhone 8"
+        case .iPhone12Pro:
+            return "iPhone 12 Pro"
+        case .iPhone12ProMax:
+            return "iPhone 12 Pro Max"
+        }
+    }
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+
+extension UIViewController {
+
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+
+    func showPreview(_ deviceType: DeviceType = .iPhone12Pro) -> some View {
+        Preview(viewController: self).previewDevice(PreviewDevice(rawValue: deviceType.name()))
+    }
+}
+#endif
